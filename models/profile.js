9c609289999ba4b173749a5360ae71f36c509d15
imcoco <imcoco@gmail.com>
2022-07-14 17:11:10 -0400

    edit profile model

--- models/profile.js
+++ models/profile.js
@@ -3,6 +3,11 @@ import mongoose from 'mongoose'
 const profileSchema = new mongoose.Schema({
   email: { type: String, required: true, lowercase: true, unique: true },
   name: String,
+  avatar: String,
+  favoriteSong: { type: mongoose.Schema.Types.ObjectId, ref: 'Song' },
+  favoriteDrink: { type: mongoose.Schema.Types.ObjectId, ref: 'Drink' },
+  boozyTunes: { type: mongoose.Schema.Types.ObjectId, ref: 'BoozyTune' }
+
 },{
   timestamps: true,
 })
