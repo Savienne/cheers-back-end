e369a890f079eb9cf69af55119fcf317a3f18f85
recipra <jmo0425@outlook.com>
2022-07-18 20:49:57 -0400

    adjust api controller and router

--- controllers/songs.js
+++ controllers/songs.js
@@ -6,9 +6,8 @@ import 'dotenv/config.js'
 const BASE_URL = 'https://www.theaudiodb.com/api/v1/json/'
 
 function songSearch(req, res) {
-  const search = req.params.search.split(', ')
-  axios.get(`${BASE_URL}${process.env.API_KEY}/searchtrack.php?s=`)
-  // axios.get(`https://www.theaudiodb.com/api/v1/json/2/search.php?s=${req.params.search}`)
+  console.log(req.query)
+  axios.get(`${BASE_URL}${process.env.API_KEY}/searchtrack.php?s=${req.query.artist}&t=${req.query.song}`)
   .then(songs => {
     res.json({songs: songs.data})
   })
--- routes/songs.js
+++ routes/songs.js
@@ -5,7 +5,7 @@ import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 const router = Router()
 
 /*---------- Public Routes ----------*/
-router.get('/:search', songsCtrl.songSearch)
+router.get('/', songsCtrl.songSearch)
 
 
 /*---------- Protected Routes ----------*/
