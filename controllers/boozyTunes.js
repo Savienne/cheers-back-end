cf71888f1bf49bb97f61ef4bc0459dc048b2bd74
imcoco <imcoco@gmail.com>
2022-07-19 23:56:51 -0400

    add delete boozyTune function

--- controllers/boozyTunes.js
+++ controllers/boozyTunes.js
@@ -2,6 +2,7 @@ import { BoozyTune } from '../models/boozyTune.js'
 
 const create = async (req, res) => {
   try {
+    req.body.author = req.user.profile
     const boozyTune = await BoozyTune.create(req.body)
     res.status(201).json(boozyTune)
   } catch (err) {
@@ -19,7 +20,19 @@ const index = async (req, res) => {
 }
 
 const deleteOne = async (req, res) => {
-  const boozyTunes = await BoozyTune.findById(req.params)
+  try {
+    const boozyTunes = await BoozyTune.findById(req.params.id)
+    if (boozyTunes.author.equals(req.user.profile)) {
+      await boozyTunes.delete()
+      res.json(boozyTunes)
+    } else {
+      res.status(500).json(err)
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+
 }
 
 export {
