fe2b5a9b780db08802a0bd7ecf882674dc0487df
imcoco <imcoco@gmail.com>
2022-07-19 23:30:25 -0400

    add boozyTune index

--- controllers/boozyTunes.js
+++ controllers/boozyTunes.js
@@ -1,6 +1,23 @@
 import { BoozyTune } from '../models/boozyTune.js'
 
+const create = async (req, res) => {
+  try {
+    const boozyTune = await BoozyTune.create(req.body)
+    res.status(201).json(boozyTune)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+}
+
+const index = async (req, res) => {
+  try {
+    const boozyTune = await BoozyTune.find({})
+    res.status(200).json(boozyTune)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+}
 
 export {
-
+  create
 }
--- routes/boozyTunes.js
+++ routes/boozyTunes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as boozyTunesCtrl from '../controllers/boozyTunes.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 
 const router = Router()
 
@@ -11,6 +12,7 @@ const router = Router()
 /*----------- PROTECTED ROUTES ------------*/
 router.use(decodeUserFromToken)
 router.post('/', checkAuth, boozyTunesCtrl.create)
+router.get('/', checkAuth, boozyTunesCtrl.index)
 
 
 export { router }
